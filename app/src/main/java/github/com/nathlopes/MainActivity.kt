21f4835eabcb127115d2fc51c073a1a1ed26d3de
Nathalia Lopes <correalopesnathali@gmail.com>
2024-09-10 19:49:33 -0300

    Implementando dependencias e plugins para acesso à base de dados

---
+++ app/src/main/java/github/com/nathlopes/MainActivity.kt
@@ -0,0 +1,46 @@
+package github.com.nathlopes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import github.com.nathlopes.ui.theme.Lista_de_comprasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Lista_de_comprasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Lista_de_comprasTheme {
+        Greeting("Android")
+    }
+}
